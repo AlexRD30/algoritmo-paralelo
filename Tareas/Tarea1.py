620e96acc9306bb62a64a1d0cee08b052d45b38b
AlexRD30 <rolandorondon.alumno.utesa.edu>
2023-10-17 15:30:14 -0400

    Fix bad repo

---
+++ Tareas/Tarea1.py
@@ -0,0 +1,11 @@
+numero=1
+sueldo=0
+total=0
+Sueldo13 = int 
+while numero <=12:
+    sueldo = int(input("Entre sueldo: "))
+    total=  total + sueldo 
+    numero +=1
+Sueldo13 =total/ 12
+print ("Total de sueldos: ", total)
+print ("Total de sueldo 13: ", Sueldo13)

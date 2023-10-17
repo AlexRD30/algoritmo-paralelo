6804c22c36a824cfa41260395836cb9ddcf83e8a
AlexRD30 <rolandorondon.alumno.utesa.edu>
2023-10-17 15:28:55 -0400

    Fix bad repo

--- algoritmo paralelo/while3.py
+++ algoritmo paralelo/while3.py
@@ -1,10 +1,11 @@
 numero=1
 sueldo=0
 total=0
-
+Sueldo13 = int 
 while numero <=12:
     sueldo = int(input("Entre sueldo: "))
-    total= total + sueldo/12
+    total=  total + sueldo 
     numero +=1
-    print ("Total de sueldos: ", total)
-   
+Sueldo13 =total/ 12
+print ("Total de sueldos: ", total)
+print ("Total de sueldo 13: ", Sueldo13)
